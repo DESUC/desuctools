e881001a817b184acb3a8e4403476d347e8ee5e1
Cristian Ayala <caayala@uc.cl>
2019-06-03 12:53:33 -0400

    Nueva función para extraer texto entre paréntesis.

--- R/helpers_variables.R
+++ R/helpers_variables.R
@@ -56,4 +56,30 @@ collapse_chr <- function(.data, .var, collapse = ", ") {
         stringr::str_c(., collapse = collapse)
 }
 
+#' Función para extraer texto presente entre paréntesis.
+#' Creado para trabajo con etiquetas de variables y extraer así el concepto
+#' preguntado.
+#'
+#' Si no encuentra texto entre paréntesis, devuelve el texto original.
+#'
+#' @name str_entre_parentesis
+#'
+#' @param text texto
+#'
+#' @return string
+#'
+#' @importFrom stringr str_extract
+#' @export
+str_entre_parentesis <- function(text){
+
+    # Extrae texto entre paréntesis
+    text_extract <- str_extract(text, "(?<=\\().*(?=\\))")
+
+    if (is.na(nchar(text_extract))) {
+        text_extract <- text
+    }
+
+    return(text_extract)
+
+}
 
