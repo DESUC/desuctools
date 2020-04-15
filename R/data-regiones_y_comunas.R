c513f10d37b7d6992af791b1a3cdc1c02af56c70
cristinamarchant <55768429+cristinamarchant@users.noreply.github.com>
2020-04-15 12:55:42 -0400

    Update data-regiones_y_comunas.R
    Se agregaron variables de características de las comunas a partir de Casen 2017 útiles para ajustes de no respuesta.

--- R/data-regiones_y_comunas.R
+++ R/data-regiones_y_comunas.R
@@ -4,7 +4,7 @@
 #' Tabla que contiene nombres y códigos de regiones y comunas de Chile,
 #' además de algunas segmentación es utilizadas habitualmente
 #'
-#' @format Un data frame con 346 filas (comunas) y 11 variables
+#' @format Un data frame con 346 filas (comunas) y 19 variables
 #'
 #' \describe{
 #'   \item{id}{orden de comunas siguiendo geografía y código comunal}
@@ -20,6 +20,12 @@
 #'   \item{comuna_extrema}{1 si comuna es extrema para INE, 2 si es extrema para DESUC}
 #'   \item{gran_santiago}{TRUE si comuna pertenece al Gran Santiago}
 #'   \item{gran_santiago_zonas}{nombre de zonas del Gran Santiago}
+#'   \item{educ_sup_com}{% hogares con algún miembro con educación superior}
+#'   \item{hacinamiento}{% hogares con hacinamiento}
+#'   \item{allegamiento_int}{% hogares con allegamiento interno}
+#'   \item{carente_ser_bas}{% hogares carentes de servicios básicos}
+#'   \item{carente_entorno}{% hogares carentes de características beneficiosas en el entorno}
+#'   \item{decil}{promedio de decir de ingresos de los hogares}
 #' }
 #'
 
