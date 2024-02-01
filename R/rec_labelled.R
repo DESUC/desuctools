bd072ff66f8f0e46c04d4c1208601e510048c62d
Cristián Ayala <caayala@gmail.com>
2024-02-01 11:58:38 -0300

    Correcciones a funcion rec_labelled.

--- R/rec_labelled.R
+++ R/rec_labelled.R
@@ -1,22 +1,22 @@
-#' Recodificación de variables labelled
+#' @title Recodificación de variables labelled
 #'
-#' @description Función para recodificar variables labelled utilizando una lista
+#' @description
+#' Recodificación de un vector labelled_haven utilizando una lista de etiquetas y niveles.
 #'
-#' @param .vec vector/variable a recodificar
-#' @param ls_niveles lista con recodificaciones.
+#' @param .vec `labelled_haven`: vector/variable a recodificar
+#' @param ls_niveles `list`: lista con recodificaciones.
 #'                   Debe cumplir un formato específico.
 #'
+#' @return vector labelled_haven
 #'
-#' @return vector labelled recodificado
-#'
-#'
-#' @import labelled
+#' @importFrom labelled labelled var_label `var_label<-`
+#' @importFrom purrr map_chr
 #' @importFrom forcats fct_recode
 #' @importFrom stringr str_match
 #'
 #' @examples
-#' # Crear un vector etiquetado
-#' x <- labelled::labelled(
+#' # vector etiquetado
+#' v_labelled <- labelled::labelled(
 #'   c(1, 2, 2, 2, 3, 4, 5, 5, 1, NA),
 #'   labels = c("Muy de acuerdo" = 1,
 #'              "De acuerdo" = 2,
@@ -26,11 +26,12 @@
 #' )
 #'
 #' # Definir la lista de recodificaciones
-#' # IMPORTANTE: el formato es "Val_final-Etiqueta = Val_inicial"
+#' # IMPORTANTE: el formato es "val_final-Etiqueta = val_inicial"
 #' # Donde:
-#' #    Val_final: serán los valores finales de las etiquetas.
+#' #    val_final: serán los valores finales de las etiquetas.
 #' #    Etiqueta: etiquetas de los valores anteriormente definidos.
-#' #    Val_inicial: categorías que se consideran para la recodificación (numéricos)
+#' #    val_inicial: categorías que se consideran para la recodificación (numéricos)
+#'
 #' ls_rec <- list(
 #'   "1-Muy de acuerdo + De acuerdo" = 1:2,
 #'   "2-Ni de acuerdo ni en desacuerdo" = 3,
@@ -38,18 +39,15 @@
 #'   "99-NA" = NA
 #' )
 #'
-#' # Aplicar rec_labelled al vector etiquetado
-#' resultado_recodificado <- rec_labelled(.vec = x, ls_niveles = ls_rec)
-#' resultado_recodificado
-#'
-#'
+#' # Recodificación de v_labelled según niveles de ls_rec
+#' rec_labelled(.vec = v_labelled,
+#'              ls_niveles = ls_rec)
 #'
 #' @export
+#'
 rec_labelled <- function(.vec, ls_niveles){
 
-  library(labelled)
-
-  # Funcion base para recodificacion
+  # Función base para codificación
   recode_list <- function(.vec,
                           ls_niveles) {
 
@@ -67,29 +65,24 @@ rec_labelled <- function(.vec, ls_niveles){
                             levels = c(names(ls_niveles)))
 
     vec_fct |>
-      `var_label<-`(var_label(.vec))
+      labelled::`var_label<-`(labelled::var_label(.vec))
   }
 
-
-  # Aplicacion de funcion base
+  # Aplicación de función base
   fct_rec <- recode_list(.vec, ls_niveles)
 
-
-  # Recodificacion de etiquetas
+  # Codificación de etiquetas
   nivel_chr <- stringr::str_match(levels(fct_rec), '(\\d*)-(.*)')
   nivel_num <- nivel_chr[, 2]
   nivel_cod <- nivel_chr[, 3]
 
-  fct_rec %>%
+  fct_rec |>
     forcats::fct_recode(!!!setNames(nivel_chr[,1],
-                           nivel_num)) %>%
-    as.character() %>%
-    as.numeric() %>%
-    labelled::labelled(labels = setNames(nivel_num %>% as.numeric(),
+                           nivel_num)) |>
+    as.character() |>
+    as.numeric() |>
+    labelled::labelled(labels = setNames(as.numeric(nivel_num),
                                          nivel_cod),
                        label = var_label(.vec))
 
 }
-
-
-
--- tests/testthat/test-rec_labelled.R
+++ tests/testthat/test-rec_labelled.R
@@ -1,9 +1,7 @@
-# ---------------------------------------------------------------------------- #
-#   Test de funcion rec_labelled
-# ---------------------------------------------------------------------------- #
+# Test de funcion rec_labelled ----
 
 # Vector
-x <- labelled(
+v_labelled <- labelled(
   c(1, 2, 2, 2, 3, 4, 5, 5, 1, NA),
   c("Muy de acuerdo" = 1,
     "De acuerdo" = 2,
@@ -18,33 +16,29 @@ ls_rec <- list("1-Muy de acuerdo + De acuerdo" = 1:2,
                "3-En desacuerdo + Muy en desacuerdo" = 4:5,
                "99-NA" = NA)
 
-
+# Ejecutar la función
+v_labelled_rec <- rec_labelled(.vec = v_labelled,
+                               ls_niveles = ls_rec)
 
 test_that("Recodificación de etiquetas", {
-  # Configuración del test (si es necesario)
 
-  # Ejecutar la función
-  vec_test <- rec_labelled(.vec = x,
-                           ls_niveles = ls_rec)
-
-  # Usar expect_* para verificar los resultados
-  expect_equal(names(attr(vec_test, "labels")),
+  expect_equal(names(attr(v_labelled_rec, "labels")),
                c("Muy de acuerdo + De acuerdo",
                  "Ni de acuerdo ni en desacuerdo",
                  "En desacuerdo + Muy en desacuerdo",
                  "NA"))
 })
 
-
 test_that("Recodificación de valores", {
-  # Configuración del test (si es necesario)
-
-  # Ejecutar la función
-  vec_test <- rec_labelled(.vec = x,
-                           ls_niveles = ls_rec)
 
   # Usar expect_* para verificar los resultados
-  expect_equal(class(vec_test),
-               c("haven_labelled", "vctrs_vctr", "double"))
+  expect_equal(as.numeric(v_labelled_rec),
+               c(1, 1, 1, 1, 2, 3, 3, 3, 1, 99))
+})
+
+test_that("Clase", {
+
+  # Usar expect_* para verificar los resultados
+  expect_s3_class(v_labelled_rec, "haven_labelled")
 })
 
